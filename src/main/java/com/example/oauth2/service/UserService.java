56f590e65a0f3d64e519dce48bcf32def7492cf5
Cesar Manuel Cruzata De la Cruz <cruzatadelacruzc@gmail.com>
2020-03-20 21:04:08 -0400

    Add findAllUsers

--- src/main/java/com/example/oauth2/service/UserService.java
+++ src/main/java/com/example/oauth2/service/UserService.java
@@ -2,6 +2,7 @@ package com.example.oauth2.service;
 
 import com.example.oauth2.domain.User;
 import com.example.oauth2.repositories.UserRepository;
+import com.example.oauth2.service.dto.UserDTO;
 import com.example.oauth2.service.mapper.UserMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,11 +10,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-
-import com.example.oauth2.service.dto.UserDTO;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Transactional
@@ -114,5 +113,16 @@ public class UserService {
             return true;
         }
         return false;
+    }
+
+
+    /**
+     * Find all Users given login
+     *
+     */
+    public List<UserDTO> findAllUsers() {
+        List<User> users = userRepository.findAll();
+        log.debug("Find All Users: {}", users);
+        return userMapper.toDto(users);
     }
 }
